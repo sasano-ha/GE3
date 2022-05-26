4f6a8aa4db19b66b36070d0b50bd0b7aac851da4
sasanonoha <k020g1406@g.neec.ac.jp>
2022-05-27 02:47:07 +0900

    カメラのクラスを追加したよ

--- Camera.h
+++ Camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:316436b8dfc8c0db71f572bb2872c469c55f0497ee01f4b449c1b0357a1bb484
-size 1635
+oid sha256:465473134043abd990c4d1b1673e1b1c5281cd2c30725c2486b09f9354fbcdd2
+size 3521
