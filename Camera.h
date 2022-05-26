659d684269b9c18c51f7064eaceaf7d93b55bb8e
sasanonoha <k020g1406@g.neec.ac.jp>
2022-05-27 02:13:38 +0900

    カメラクラスを追加

---
+++ Camera.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <DirectXMath.h>
+
+class Camera
+{
+private: // エイリアス
+// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+
+public:
+	/// <summary>
+	/// カメラ初期化
+	/// </summary>
+	/// <param name="window_width">画面横幅</param>
+	/// <param name="window_height">画面縦幅</param>
+	static void Initialize(int window_width, int window_height);
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// ビュー行列を更新
+	/// </summary>
+	static void UpdateViewMatrix();
+
+	/// <summary>
+	/// 視点座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	static const XMFLOAT3& GetEye() { return eye; }
+
+	/// <summary>
+	/// 視点座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	static void SetEye(XMFLOAT3 eye);
+
+	/// <summary>
+	/// 注視点座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	static const XMFLOAT3& GetTarget() { return target; }
+
+	/// <summary>
+	/// 注視点座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	static void SetTarget(const XMFLOAT3& target);
+
+	/// <summary>
+	/// ベクトルによる移動
+	/// </summary>
+	/// <param name="move">移動量</param>
+	static void CameraMoveVector(XMFLOAT3 move);
+
+private:
+	// ビュー行列
+	static XMMATRIX matView;
+	// 射影行列
+	static XMMATRIX matProjection;
+
+	static XMMATRIX matPerspective;
+	// 視点座標
+	static XMFLOAT3 eye;
+	// 注視点座標
+	static XMFLOAT3 target;
+	// 上方向ベクトル
+	static XMFLOAT3 up;
+};
